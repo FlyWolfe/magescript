7adc32c098d9722b33d285361a23f3cbbb5601c5
mathew-j-sherry <mathew.sherry@serenityforge.com>
2021-02-25 01:51:45 -0500

    Initial project commit with almost no code

---
+++ MageLang/lexer.cpp
@@ -0,0 +1,14 @@
+#include "lexer.h"
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+vector<string> Lexer::LexLine(string line)
+{
+	vector <string> tokens;
+
+
+	return tokens; 
+}
---
+++ MageLang/lexer.h
@@ -0,0 +1,6 @@
+#pragma once
+
+class Lexer
+{
+	vector<string> LexLine(string line);
+};
