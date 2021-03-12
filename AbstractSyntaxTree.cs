998282cd010c881afd2342090cd0711f150202f3
FlyWolfe <mjsherry248@gmail.com>
2021-03-12 03:25:14 -0500

    Getting started on the Abstract Syntax Tree and Parser And a formatting change to the Token Class

---
+++ AbstractSyntaxTree.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace magelang
+{
+    abstract class Statement { }
+    abstract class Expression { }
+
+    class Block : Statement
+    {
+        public List<Statement> statements;
+
+        public Block()
+        {
+            statements = new List<Statement>();
+        }
+
+        public void AddSatement(Statement statement)
+        {
+            statements.Add(statement);
+        }
+    }
+
+    class Function : Block
+    {
+        public string identifier;
+        public List<string> vars;
+
+        public Function(string identifier, List<string> vars)
+        {
+            this.identifier = identifier;
+            this.vars = vars;
+        }
+    }
+
+    class Repeater : Block
+    {
+        public string identifier;
+        public List<string> vars;
+
+        public Repeater(string identifier, List<string> vars)
+        {
+            this.identifier = identifier;
+            this.vars = vars;
+        }
+    }
+
+    class IfBlock : Block
+    {
+        public Expression leftExpr;
+        // TODO: public Symbol op;
+        public Expression rightExpr;
+
+        public IfBlock(Expression leftExpr, /* TODO: Symbol o,*/ Expression rightExpr)
+        {
+            this.leftExpr = leftExpr;
+            // TODO: op = o;
+            this.rightExpr = rightExpr;
+        }
+    }
+
+    class ElseIfBlock : Block
+    {
+        public Expression leftExpr;
+        // TODO: public Symbol op;
+        public Expression rightExpr;
+
+        public ElseIfBlock(Expression leftExpr, /* TODO: Symbol o,*/ Expression rightExpr)
+        {
+            this.leftExpr = leftExpr;
+            // TODO: op = o;
+            this.rightExpr = rightExpr;
+        }
+    }
+
+    class ElseBlock : Block { }
+
+    class EndIf : Block { }
+
+    class Assign : Statement
+    {
+        public string identifier;
+        public Expression value;
+
+        public Assign(string identifier, Expression value)
+        {
+            this.identifier = identifier;
+            this.value = value;
+        }
+    }
+}
---
+++ Parser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace magelang
+{
+    class Parser
+    {
+        static TokenList tokens;
+        // TODO: static Block currentBlock;
+        // TODO: static Stack<Block> blockstack;
+        // TODO: static List<Stmt> tree;
+        static bool running;
+
+        public Parser(TokenList t)
+        {
+            tokens = t;
+            // TODO: currentBlock = null;
+            // TODO: blockstack = new Stack<Block>();
+            Token tok = null;
+            // TODO: tree = new List<Stmt>();
+            running = true;
+
+            while (running)
+            {
+                // TODO: Add things here
+            }
+        }
+    }
+}
